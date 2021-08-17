2d4d5f11690dcdc094a0451978fe11ac848183d4
Rex Ching <rex@v-promo.com>
2021-08-17 15:53:28 -0700

    e2e demo

---
+++ end-to-end/packages/react-app/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import { Image, PageHeader } from "antd";
+import React from "react";
+
+// displays a page header
+
+export default function Header() {
+  return (
+
+    <a href="http://mobs.video" target="_blank" rel="noopener noreferrer">
+      <PageHeader
+        title="" 
+        style={{ cursor: "pointer" ,background:"white" }}
+        
+      />
+            <Image background="black" src="https://cdn.shopify.com/s/files/1/0373/7957/1848/files/MOBS-LOGO-website_200x.png?v=1598435308" 
+                    style={{ cursor: "pointer" ,background:"black" }}
+            />
+
+    </a>
+  );
+}
